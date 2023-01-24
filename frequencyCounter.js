3c47c761a69a2e88c0f6c04963fce87ed7d206ea
Ryan Migaud <rmigaud@asu.edu>
2023-01-24 15:22:04 -0800

    refactored

--- frequencyCounter.js
+++ frequencyCounter.js
@@ -1,34 +1,31 @@
 const frequencyCounter = (arr1, arr2) => {
-    if (arr1.length != arr2.length) return false;
+  if (arr1.length != arr2.length) return false;
 
-    let histogram1 = {};
-    let histogram2 = {};
+  let histogram1 = newHistogram(arr1);
+  let histogram2 = newHistogram(arr2);
 
-    for(let elm of arr1) {
-        if(histogram1.hasOwnProperty(elm)) {
-            histogram1[elm] += 1;
-        } else {   
-            histogram1[elm] = 1;
-        }
+  for (let count in histogram1) {
+    if (!(count in histogram2)) {
+      return false;
+    } else {
+      if (histogram2[count] != histogram1[count]) {
+        return false;
+      }
     }
+  }
+  return true;
+};
 
-    for(let elm of arr2) {
-        histogram2[elm] = (histogram2[elm] || 0) + 1;
+const newHistogram = (arr1) => {
+  let histogram = {};
+  for (let elm of arr1) {
+    if (histogram.hasOwnProperty(elm)) {
+      histogram[elm] += 1;
+    } else {
+      histogram[elm] = 1;
     }
+  }
+  return histogram;
+};
 
-    for(let count in histogram1) {
-        if(!(count in histogram2)){
-            return false;
-        } else {
-            // console.log(count, "{" + histogram1[count] , histogram2[count] + "}");
-            if(histogram2[count] != histogram1[count]) {
-                return false;
-            }
-        } 
-    }
-    return true;
-}
-
-// frequencyCounter([1,1,1,2,3,3], [3,4,2,1,1,1]); // false
-frequencyCounter([1,2,2,3], [3,1,2,2]); // true
-frequencyCounter("hello".split(),"hello".split().reverse()) // true
+frequencyCounter("hello".split(), "hello".split().reverse()); // true
